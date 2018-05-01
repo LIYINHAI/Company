fd9b6435893e91f412eddb2473b5211d25ac2274
LIYINHAI <17770849628@163.com>
2018-05-01 12:01:10 +0800

    001

---
+++ Company.DAL/BaseDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//四个引用
+using Company.IDAL;
+using System.Linq.Expressions;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace Company.DAL
+{
+   public partial class BaseDAL<T> where T :class,new()
+    {
+        //对IBaseDAL的具体实现
+        private DbContext dbcontext =DbContextFactory.Create();
+        //增加
+        public void Add(T t)
+        {
+            dbcontext.Set<T>().Add(t);
+        }
+        //删除
+        public void Delect(T t)
+        {
+            dbcontext.Set<T>().Remove(t);
+        }
+        //修改
+        public void Update(T t)
+        {
+            dbcontext.Set<T>().AddOrUpdate(t);
+        }
+        //页面
+        public IQueryable<T> GetModels(Expression<Func<T, bool>> whereLambda)
+        {
+            return dbcontext.Set<T>().Where(whereLambda);
+        }
+        public IQueryable<T> GetModelsByPage<type>(int pageSize, int pageIndex, bool isAsc,
+            Expression<Func<T, type>> OrderByLambda, Expression<Func<T, bool>> whereLamdba)
+        {
+            //是否升序
+            if (isAsc)
+            {
+                return dbcontext.Set<T>
+            ().Where(whereLamdba).OrderBy(OrderByLambda).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            }
+            else
+            {
+                return dbcontext.Set<T>().Where(whereLamdba).OrderByDescending(OrderByLambda).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            }
+        }
+        public bool SaveChanges()
+        {
+            return dbcontext.SaveChanges() > 0;
+        }
+    }
+}
---
+++ Company.IDAL/IBaseDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.IDAL
+{
+   public partial interface IBaseDAL<T> where T :class,new()
+    {
+        ///公用的方法签名
+        /// <summary>
+        /// 增加
+        /// </summary>
+        /// <param name="t"></param>
+        void Add(T t);
+        //删除
+        void Delete(T t);
+        //更新
+        void Updete(T t);
+        //Ling语句
+        IQueryable<T> GetModels(Expression<Func<T, bool>> whereLambda);
+        IQueryable<T> GetModelsByPage<type>(int pageSize, int pageIndex, bool isAsc,
+            Expression<Func<T, type>> OrderByLambda, Expression<Func<T, bool>> whereLamdba);
+        /// <summary>
+        /// 一个业务中有可能涉及到对多张表的操作,那么可以将操作的数据,打上相应的标记,最后调用该方法,
+        /// 将数据一次性提交到数据库中,避免了多次链接数据库。
+        /// </summary>
+        /// <returns></returns>
+        bool SaveChanges();
+    }
+}
